49b10b7ed3cbf0fd544d70859fac840018bb0a01
Al Mamun <mamuncse82@gmail.com>
2023-03-15 00:46:21 +0600

    todo_list

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const inputfield = document.getElementById('inputfield');
+const inputsubmit = document.getElementById('inputsubmit');
+const Display = document.getElementById('display');
+
+inputsubmit.addEventListener("click", ()=>{
+    
+})
