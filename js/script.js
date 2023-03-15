4143ca839f06bcd440b2b63dde10c758648a1de4
Al Mamun <mamuncse82@gmail.com>
2023-03-15 13:01:20 +0600

    complete

--- js/script.js
+++ js/script.js
@@ -2,6 +2,17 @@ const inputfield = document.getElementById('inputfield');
 const inputsubmit = document.getElementById('inputsubmit');
 const Display = document.getElementById('display');
 
-inputsubmit.addEventListener("click", ()=>{
-    
+inputsubmit.addEventListener("click", (e)=>{
+  e.preventDefault()
+    const list = document.createElement('p');
+    list.classList.add('list-style')
+    list.innerText = inputfield.value;
+    Display.appendChild(list);
+    inputfield.value = "";
+    list.addEventListener("click", ()=>{
+      list.style.textDecoration = 'line-through';
+    })
+    list.addEventListener("dblclick", ()=>{
+      Display.removeChild(list);
+    })
 })
